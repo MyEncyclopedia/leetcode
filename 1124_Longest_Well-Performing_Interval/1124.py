d058419ee597b1c6da4012678e458c3d7c8d3637
Frank <dingding303@gmail.com>
2020-07-22 21:35:18 +0800

    two more problems

---
+++ 1124_Longest_Well-Performing_Interval/1124.py
@@ -0,0 +1,4 @@
+from typing import List
+
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
---
+++ 1131_Maximum_of_Absolute_Value_Expression/1131.py
@@ -0,0 +1,4 @@
+from typing import List
+
+class Solution:
+    def maxAbsValExpr(self, arr1: List[int], arr2: List[int]) -> int:
